bdfbab758489e59429cf9f7aee5219c8e055a529
Takanori Onuma <s1000607@sendai-nct.jp>
2014-01-03 10:21:51 +0900

    smartyの使用
    smartyを使用してlogin.htmlを表示するようにした。
    
    Signed-off-by: Takanori Onuma <s1000607@sendai-nct.jp>

--- login.php
+++ login.php
@@ -1,4 +1,19 @@
 <?php
+    // テンプレート利用準備
+    require_once 'smarty/Smarty.class.php';
+
+    $smarty = new Smarty();
+    $smarty->template_dir = 'templates/';
+    $smarty->compile_dir  = 'templates_c/';
+
+    // フォーム送信でない時（ログインするために来た時）
+    if (!isset($_POST['id'])) {
+        $smarty->assign('error_message', '');
+        $smarty->display('login.html');
+        exit;
+    }
+
+    // フォーム送信で来たとき
     $auto_login_flag = (isset($_POST['auto_login'])) ? 'true' : 'false';
 
     echo <<<EOM
