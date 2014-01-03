2f21fc0a7414819d10deddec6b19b777515b839a
Takanori Onuma <s1000607@sendai-nct.jp>
2014-01-03 10:39:30 +0900

    ログイン認証のチェック
    ログイン認証のチェックを行った。
    今の段階ではSHA1を使わずに認証している。
    
    Signed-off-by: Takanori Onuma <s1000607@sendai-nct.jp>

--- login.php
+++ login.php
@@ -14,6 +14,38 @@
     }
 
     // フォーム送信で来たとき
+    try {
+        $pdo = new PDO('mysql:dbname=phpdb;host=127.0.0.1', 'root', 'ayashi', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+        $pdo->query('SET NAMES utf8');
+
+        $stmt = $pdo->prepare('SELECT * FROM accounts WHERE id = :id');
+        $stmt->bindValue(':id', $_POST['id']);
+        $stmt->execute();
+
+        $error_message = '';        // エラーなしと宣言しておく
+        // エラーチェック
+        $data = $stmt->fetch(PDO::FETCH_ASSOC);
+        if ($data == null) {
+            $error_message .= "このIDは使われていません。\n";
+        }
+        else {
+            if ($_POST['password'] != $data['password']) {
+                $error_message .= "パスワードが正しくありません。\n";
+            }
+        }
+
+        // エラーがあったかを見る
+        if ($error_message != '') {
+            $smarty->assign('error_message', '<h2>' . nl2br($error_message) . '</h2>');
+            $smarty->display('login.html');
+            exit;
+        }
+    }
+    catch(PDOException $e) {
+        exit($e->getMessage());
+    }
+
+    // エラーがない時
     $auto_login_flag = (isset($_POST['auto_login'])) ? 'true' : 'false';
 
     echo <<<EOM
