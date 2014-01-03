2ffc2de3d4079a8c14cf2d85b4e60dd91712d8c8
Takanori Onuma <s1000607@sendai-nct.jp>
2014-01-03 22:17:20 +0900

    IDやPWなどをprivateにした。
    定数を使ってPDOを生成するつもりだったが、myDataBaseでPDOの生成まで行うため、
    定数をpublicにする必要はなかった。そのため、定数をprivateに変えた。
    （言い方に多少語弊があるが、実際はprivateにすると定数という扱いが出来なくなる。
    しかしクラス内で変数をいじらなければ実質定数なのでそのような言い方になっている。）
    
    Signed-off-by: Takanori Onuma <s1000607@sendai-nct.jp>

--- myDataBase.php
+++ myDataBase.php
@@ -1,12 +1,12 @@
 <?php
     final class myDataBase {
-        const DB_NAME = 'phpdb';
-        const IP_ADDRESS = '127.0.0.1';
-        const ID = 'root';
-        const PW = 'ayashi';
+        private static $DB_NAME = 'phpdb';
+        private static $IP_ADDRESS = '127.0.0.1';
+        private static $ID = 'root';
+        private static $PW = 'ayashi';
 
         public static function createPDO() {
-            return new PDO('mysql:dbname=' . myDataBase::DB_NAME . ';host=' . myDataBase::IP_ADDRESS, myDataBase::ID, myDataBase::PW, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+            return new PDO('mysql:dbname=' . self::$DB_NAME . ';host=' . self::$IP_ADDRESS, self::$ID, self::$PW, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
         } 
     }
 ?>
