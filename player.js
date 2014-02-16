34a9b07c9d4ccfffe255ef39334e69e1bef2ed16
Takanori Onuma <s1000607@sendai-nct.jp>
2014-02-16 10:03:35 +0900

    色情報のプロパティを入れた
    プレイヤーがどっちの色（黒か白）なのか分かるようにクラスの中に入れた。
    
    Signed-off-by: Takanori Onuma <s1000607@sendai-nct.jp>

--- player.js
+++ player.js
@@ -2,18 +2,19 @@
 //** オセロのプレイヤーをクラス化する **
 //************************************************
 // --- Playerクラス ---//
-function Player() {
+function Player(color) {
+	this.color = color;		// どっちの石か（黒(0)か白(1)か）
 }
 
 // inputメソッドの空定義
 Player.prototype.input = function(i) {
-	alert('This is player class.');
+	alert("This is player class.\n" + "color:" + this.color);
 };
 
 // --- Humanクラス（人がプレイヤー） --- //
-function Human() {
+function Human(color) {
 	// 親クラスPlayerのコンストラクタを呼ぶ
-	Player.call(this);
+	Player.call(this, color);
 }
 
 // Playerのメソッドを継承する
@@ -22,5 +23,5 @@ Human.prototype = new Player();
 Human.prototype.constructor = Human;
 // inputメソッドのオーバーライド
 Human.prototype.input = function(i) {
-	alert("This is Human class.");
+	alert("This is Human class.\n" + "color:" + this.color);
 }
